35ccdeece26752d8b9c7639a7788699ce661369f
lbatalha <lfrb25@gmail.com>
2015-01-21 23:49:10 +0000

    Init

---
+++ server.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import socket
+
+HOST = ''                 
+PORT = 8080
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind((HOST, PORT))
+s.listen(1)
+conn, addr = s.accept()
+print('Connected by', addr)
+while True:
+	data = conn.recv(1024)
+	if not data: break
+	conn.sendall(b'asdasdasdasd\n')
+conn.close()
