bcfc97af4e897b802e0129ae9776164460407ae8
lbatalha <lfrb25@gmail.com>
2015-01-22 00:33:27 +0000

    add html

--- server.py
+++ server.py
@@ -4,6 +4,11 @@ import socket
 
 HOST = ''                 
 PORT = 8080
+
+fo = open('index.html', 'r')
+content = fo.read()
+message = "HTTP/1.1 200 OK\n\n" + content
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((HOST, PORT))
@@ -13,5 +18,6 @@ print('Connected by', addr)
 while True:
 	data = conn.recv(1024)
 	if not data: break
-	conn.sendall(b'asdasdasdasd\n')
+	conn.sendall(bytes(message, 'UTF-8'))
 conn.close()
+
